71e13dc262ce8da6eb3a39d77edae3c307dac284
karygauss03 <karim.omrane@supcom.tn>
2024-10-20 00:13:22 +0100

    last path

--- Web_Interface/graphics/figures.py
+++ Web_Interface/graphics/figures.py
@@ -1,8 +1,8 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("./Web_interface/data/cidals_final_2.csv")
-df_full = pd.read_csv("./Web_interface/data/cidals_full_view.csv")
+df = pd.read_csv("./Web_Interface/data/cidals_final_2.csv")
+df_full = pd.read_csv("./Web_Interface/data/cidals_full_view.csv")
 df_unique_smiles = df.drop_duplicates(subset="SMILES")
 unique_values = df_unique_smiles["Biological Activity"]
 value_counts = pd.Series(unique_values).value_counts()
