112858261815d72c3273205f00f5804e780bc89c
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-10-25 00:23:47 +0100

    a

--- Web_Interface/predict.py
+++ Web_Interface/predict.py
@@ -111,8 +111,8 @@ def predict_with_model(smile, model_path):
         # Make the prediction using the loaded model
         y = model.predict([x])
         z = model.predict_proba([x])
-        # if (model_path == './Web_Interface/models/Coronavirus_GB.pkl'):
-        #     z = z[0]
+        if (model_path == './Web_Interface/models/Coronavirus_GB.pkl'):
+            y = y[0]
     return [y, z] 
 
 def pubchem_id_to_smiles(pubchem_id):
@@ -235,12 +235,8 @@ def predict():
                     with col2:
                         with st.spinner(progress_text):
                             [res, proba] = predict_with_model(smile, f"./Web_Interface/models/{option}.pkl")
-                            st.text(res)
-                            st.text(proba)
                             active_proba = proba[0][1]
                             inactive_proba = proba[0][0]
-                            st.text(active_proba)
-                            st.text(inactive_proba)
                             if res == 1:
                                 add_vertical_space(4)
                                 st.success(f'Active (Probability: {active_proba:.2f})', icon="✅")
